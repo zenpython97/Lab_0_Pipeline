66a3b737af9ef8f9b5f71d3e7dbe25c4ff970592
zenpython97 <williams_kamgatamgno@student.uml.edu>
2023-03-03 19:43:50 -0500

    demo project added

---
+++ src/test/java/com/willliams/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willliams.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
